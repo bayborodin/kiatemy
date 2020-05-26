d4678b4b236f1b4eaebaaeee048840ce5378d8b3
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-05-26 18:55:54 +1000

    Dockerize application

--- api/kiatemy/settings.py
+++ api/kiatemy/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x!o!!-40abya-#@n%dm5n*!_04$wn2bc3*8a*3itok-upsj@sj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
