1d22dc7a724850857572d8efe0638fa8900f9a6a
Nicholas Bayborodin <nicholas.bayborodin@gmail.com>
2020-06-26 16:01:30 +1000

    Add Account name field

--- api/account/tests/test_models.py
+++ api/account/tests/test_models.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
 
+from account import models
+
 
 class ModelTests(TestCase):
     def test_create_account(self):
-        """Test creating a new account is successful"""
-        pass
+        """Test Account string representation"""
+        account = models.Account.objects.create(
+            name="Test Account"
+        )
+
+        self.assertEqual(str(account), account.name)
